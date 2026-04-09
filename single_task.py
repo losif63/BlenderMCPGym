0e4f718a92540b963b0ab402239ca028655d45c0
losif63 <jaduksuh@naver.com>
2026-04-09 12:52:32 -0700

    Automate blender opening file and turn on blendermcp server

---
+++ single_task.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+import os
+import subprocess
+
+
+def main(args):
+    blend_file = os.path.expanduser(f"~/Desktop/Research/BlenderMCPGym/bench_data/{args.task_name}/blender_file.blend")
+    subprocess.Popen([
+        "/Applications/Blender.app/Contents/MacOS/blender",
+        blend_file,
+        "--python-expr",
+        "import bpy; bpy.app.timers.register(lambda: bpy.ops.blendermcp.start_server(), first_interval=1.0)"
+    ])
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--task_name', type=str, default="blendshape1")
+    args = parser.parse_args()
+
+    main(args)
+
