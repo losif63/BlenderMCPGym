aff4d7490e6107dc666575887c2b2772e4e8c99f
losif63 <jaduksuh@naver.com>
2026-04-09 21:30:10 -0700

    Add blender file copying

--- single_task.py
+++ single_task.py
@@ -1,13 +1,16 @@
 from argparse import ArgumentParser
 import os
+import shutil
 import subprocess
 
 
 def main(args):
     blend_file = os.path.expanduser(f"~/Desktop/Research/BlenderMCPGym/bench_data/{args.task_name}/blender_file.blend")
+    edit_file = os.path.expanduser(f"~/Desktop/Research/BlenderMCPGym/bench_data/{args.task_name}/edit.blend")
+    shutil.copy2(blend_file, edit_file)
     subprocess.Popen([
         "/Applications/Blender.app/Contents/MacOS/blender",
-        blend_file,
+        edit_file,
         "--python-expr",
         "import bpy; bpy.app.timers.register(lambda: bpy.ops.blendermcp.start_server(), first_interval=1.0)"
     ])
